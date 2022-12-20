e0458cb15e9693b5cf46114daa2c5a3f0b0580d1
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2022-12-20 23:17:51 +0100

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -30,12 +30,12 @@ def export_products_csv(request):
     response['Content-Disposition'] = 'attachment; filename="products.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['ID', 'Title', 'Description', 'Link', 'Image Link', 'Availability', 'Price', 'Condition', 'Brand', 'Currency'])
+    writer.writerow(['ID', 'Title', 'Description', 'Link', 'Image Link', 'Availability', 'Price', 'Condition', 'Brand'])
 
     for product in products:
         content = strip_tags(product.content).replace('&nbsp;', '')
         content = os.linesep.join([s for s in content.splitlines() if s])
-        writer.writerow([product.id, product.title, content , 'https://greengoshop.mk' + product.get_absolute_url(), 'https://greengoshop.mk' + product.thumbnail.url, 'in stock', product.sale_price + 'MKD', 'New', 'GreenGoShopMK', 'MKD'])
+        writer.writerow([product.id, product.title, content , 'https://greengoshop.mk' + product.get_absolute_url(), 'https://greengoshop.mk' + product.thumbnail.url, 'in stock', str(product.sale_price) + 'MKD', 'New', 'GreenGoShopMK'])
 
     return response
 
