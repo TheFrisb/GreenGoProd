246b7fceddc0e02523de87f2192fcd95d927e243
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2022-12-26 05:17:05 +0100

    Update slider.js

--- shop/static/shop/js/slider.js
+++ shop/static/shop/js/slider.js
@@ -7,6 +7,31 @@ $(window).on("load", function () {
         speed: 200,
         
     });
+    if(window.matchMedia("(min-width: 767px)").matches){
+        $($(".slick-slide img").get().reverse()).each(function(i, el){
+            if(!$(this).closest(".slick-slide").hasClass("slick-cloned")){
+                $img = $(this).clone();
+                if($(this).closest(".slick-slide").hasClass("slick-current")){
+                    $img.addClass("active-gallery-item");
+                    console.log("WAS")
+                }
+                $img.height(50); 
+                $img.width(50);
+                $img.prependTo($('.gallery-navigation'));
+    
+            }
+        });
+        gallery_navigation_images = $(".gallery-navigation img")
+        $(".gallery-navigation img").on("click", function(){
+            $(".gallery-navigation img").removeClass("active-gallery-item")
+            $(".slider-holder").slick('slickGoTo', $(this).index());
+            $(this).addClass("active-gallery-item");
+        })
+        $('.slider-holder').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            $(gallery_navigation_images).removeClass("active-gallery-item")
+            $(gallery_navigation_images[nextSlide]).addClass("active-gallery-item");
+          });
+    }
     $('.slider-holder').show();
     $("#overlay-loader").hide();
     $("#slider2").slick({
