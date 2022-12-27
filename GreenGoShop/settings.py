c0939f292e7595aff2d223252b69992bf8304ce6
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2022-12-27 22:20:03 +0100

    Update settings.py

--- GreenGoShop/settings.py
+++ GreenGoShop/settings.py
@@ -119,7 +119,35 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/home/sammy/myprojectdir/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
 
 
 LANGUAGE_CODE = 'en-us'
