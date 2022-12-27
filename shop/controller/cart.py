ff966fd557e8f80b0940c497d401916f7a1b1256
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2022-12-27 04:19:56 +0100

    Update cart.py

--- shop/controller/cart.py
+++ shop/controller/cart.py
@@ -19,10 +19,10 @@ def addtocart(request):
                     cartItem.product_qty = cartItem.product_qty + prod_qty
                     cartItem.save()
                     
-#                     try:
-                    facebook_pixel.AddToCartPixelEvent(request, 'NORMAL', product_check, prod_qty)
-#                     except:
-#                         pass
+                    try:
+                        facebook_pixel.AddToCartPixelEvent(request, 'NORMAL', product_check, prod_qty)
+                    except:
+                         pass
                     return JsonResponse({'status': "Product added with quantity saved"})
                 else:
                     # Check Stock
@@ -31,7 +31,7 @@ def addtocart(request):
                     CartItems.objects.create(cart = CartHolder, product_id=prod_id, product_qty=prod_qty)
                     
 #                     try:
-                        facebook_pixel.AddToCartPixelEvent(request, 'NORMAL', product_check, prod_qty)
+                    facebook_pixel.AddToCartPixelEvent(request, 'NORMAL', product_check, prod_qty)
 #                     except:
 #                         pass
                 
