ec40659c474acdbba49d7f5e861257c62004f932
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 20:23:14 +0100

    Update models.py

--- analytics/models.py
+++ analytics/models.py
@@ -1,1 +1,15 @@
 
+from django.db import models
+from shop.models import Product, OrderItem
+import datetime
+from django.urls import reverse
+from django.utils import timezone
+# Create your models here.
+
+
+class daily_items(models.Model):
+    pass
+
+class daily_row(models.Model):
+    pass
+
