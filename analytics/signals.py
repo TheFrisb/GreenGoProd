bc81b99d2469b9adb357109dd24c0bc3163066d8
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 21:25:43 +0100

    Delete analytics directory

--- analytics/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import *
-from shop.models import Product
-
-
-@receiver(post_save, sender=daily_items)
-def update_daily_item_slug(sender, instance, created, **kwargs):
-    daily_items.objects.filter(id=instance.pk).update(slug=instance.id)
