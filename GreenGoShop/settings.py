9f2749bf8847214f533e61b2b4fe7b9778675f67
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 21:34:14 +0100

    Update settings.py

--- GreenGoShop/settings.py
+++ GreenGoShop/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['www.greengoshop.mk', 'greengoshop.mk', '161.35.16.191'] 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
-    'analytics.apps.AnalyticsConfig',
+    'analytics.apps.AnalyticsCofig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
