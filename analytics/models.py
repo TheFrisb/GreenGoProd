43e8fe61953be23adf53655bac74c2d16158f214
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 20:24:28 +0100

    Update models.py

--- analytics/models.py
+++ analytics/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from shop.models import Product, OrderItem
 import datetime
@@ -8,8 +7,58 @@ from django.utils import timezone
 
 
 class daily_items(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, verbose_name='Продукт')
+    slug =  models.SlugField(unique=True, max_length=250, blank = True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('daily_ad_spend_by_id', args=[str(self.slug)])
+    
+    def __str__(self):
+        return 'Item - {} {}'.format(self.product.sku, self.slug)
+
 
 class daily_row(models.Model):
-    pass
+    owner = models.ForeignKey(daily_items, on_delete=models.SET_NULL, null=True, verbose_name='Дневен ред на информации')
+    quantity = models.IntegerField(verbose_name='Количина', null=True, blank=True)
+    price = models.IntegerField(verbose_name='Цена на продукт', null=True, blank=True,)
+    neto_price = models.IntegerField(verbose_name='Нето вредност', null=True, blank=True,)
+    stock_price = models.IntegerField(verbose_name='Набавка', null=True, blank=True,)
+    fixed_cost = models.IntegerField(verbose_name='Фиксен трошок', null=True, blank=True)
+    ad_cost = models.FloatField(verbose_name='Трошок за реклама', null=True, blank=True,)
+    neto_total = models.IntegerField(verbose_name='NETO total', null=True, blank=True)
+    profit = models.IntegerField(verbose_name='Профит(ЌЕШ)', null=True, blank=True,)
+    cost_per_purchase = models.FloatField(verbose_name='Cost per purchase', null=True, blank=True,)
+    be_roas = models.FloatField(verbose_name='BE ROAS', null=True, blank=True,)
+    roas = models.FloatField(verbose_name='ROAS', null=True,  blank=True,)
+    roi = models.FloatField(verbose_name='ROI', null=True)
+    comment = models.TextField(null=True, blank=True, verbose_name='Коментар')
+
+    created_at = models.DateTimeField(default=timezone.now, editable=True, verbose_name='Креиран во')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Упдејтиран/Импортиран во')
+
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+           pass
+        else:
+            self.neto_total = self.quantity * self.neto_price
+            self.profit = self.neto_total - self.ad_cost
+            if(self.quantity != 0):
+                self.cost_per_purchase = self.ad_cost / self.quantity
+            else:
+                self.cost_per_purchase = self.ad_cost
+            self.be_roas = self.price / self.neto_price
+            if(self.ad_cost!=0):
+                self.roas = (self.quantity * self.price) / (self.ad_cost)
+                self.roi = (self.neto_price * self.quantity) / (self.ad_cost)
+            else:
+                self.roas = (self.quantity * self.price)
+                self.roi = (self.neto_price * self.quantity)
+            print('Save called')
+        super(daily_row, self).save(*args, **kwargs)
+
+
+
+    def __str__(self):
+        return 'Daily row за - {}'.format(self.owner)
 
