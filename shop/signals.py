c3248b3ecb64002b3490dcd9aa22b1fc2c4272ab
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 21:50:10 +0100

    Update signals.py

--- shop/signals.py
+++ shop/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Review, Product, ProductAttribute, product_campaigns
+from analytics.models import *
 
 
 
