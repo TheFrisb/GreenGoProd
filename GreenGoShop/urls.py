aa0f778fd801e36b6037209ab8210ea1d4665f99
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-01-31 21:26:39 +0100

    Update urls.py

--- GreenGoShop/urls.py
+++ GreenGoShop/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('analytics/', include('analytics.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
