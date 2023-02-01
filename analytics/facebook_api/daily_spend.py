4338cfb722fbdc82fcd00682b97341be4690633b
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-02-01 06:01:53 +0100

    Update daily_spend.py

--- analytics/facebook_api/daily_spend.py
+++ analytics/facebook_api/daily_spend.py
@@ -8,8 +8,10 @@ from analytics.models import *
 from datetime import datetime
 from shop.models import product_campaigns, Product
 from decouple import config
+import logging
 
 
+logger = logging.getLogger(__file__)
 def get_campaign_id():
     # Initialize the Facebook Ads SDK with the access token
     FacebookAdsApi.init(access_token=config('MARKETING_API_SECRET_KEY'))
