6d0a1f0bb20c9bc4cbc2806fb30d589f08edfc32
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-02-01 04:28:42 +0100

    Create cron.py

---
+++ shop/cron.py
@@ -0,0 +1,8 @@
+from .models import Cart
+from datetime import datetime, timedelta
+import datetime
+from django.utils import timezone
+
+def old_carts_cleaner():
+    two_days_ago = timezone.now() - timezone.timedelta(days=2)
+    Cart.objects.filter(created_at__gt=two_days_ago).delete()
