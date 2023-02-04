485428df853c9af1db36c65738d400bac7f40cc8
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-02-04 04:13:21 +0100

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -111,7 +111,7 @@ class Product(models.Model):
 class product_campaigns(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, verbose_name='Продукт')
     title = models.TextField(null=True, blank=True)
-
+    campaign_id = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.product.title
     
