42fe246a76d68e5dd37e333552cce00f4b1c9384
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-02-28 03:09:46 +0100

    Update urls.py

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('get_ad_spend_by_date', views.get_ad_spend_by_date, name='get_ad_spend_by_date'),
     path('daily_ad_spend/<int:pk>/', views.daily_ad_spend_by_id, name='daily_ad_spend_by_id'),
     path('daily_ad_spend/', views.daily_ad_spend, name='daily_ad_spend'),
     path('login/', views.user_login, name='login'),
