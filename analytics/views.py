14ea9a4e136236a7373e10688be2edb7c01f3e05
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-03-30 12:59:12 +0200

    Update views.py

--- analytics/views.py
+++ analytics/views.py
@@ -3,14 +3,21 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import *
+from shop.models import Product, product_campaigns
 import json
 from django.http import HttpResponse, JsonResponse
-from .facebook_api import daily_spend
+from .facebook_api import daily_spend, ad_campaigns
 from datetime import datetime, timedelta
 from shop.models import Product
 import requests
 import logging
 from decouple import config
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
+import random
+from django.conf import settings
+from PIL import Image
+from io import BytesIO
 
 
 logger = logging.getLogger(__file__)
@@ -273,3 +280,231 @@ def retrieve_adspend(request):
 
     
    
+def create_new_ad(request):
+    context={
+        'products': Product.objects.filter(status__in=['PUBLISHED', 'VARIABLE']),
+    }
+    print('yeah')
+    #ad_campaigns.create_ad_campaign()
+    return render(request, 'analytics/create_new_ad.html', context)
+
+
+def get_product(request):
+    if request.method == 'GET':
+        #get product id from request
+        product_id = request.GET.get('product_id')
+        
+        product = Product.objects.get(id=product_id)
+        return JsonResponse({
+            'url': product.get_absolute_url(),
+            'thumbnail': product.thumbnail.url,
+            'title': product.title,
+            'regular_price': product.regular_price,
+            'sale_price': product.sale_price,
+            'label': product.sku
+            })
+    else:
+        print('whoops')
+        return redirect('/')
+
+
+
+def upload_campaign_photo(request):
+    if request.method == 'POST':
+        ad_image = request.FILES['ad_image']
+        ad_image_name = ad_image.name
+        
+        # date_today = datetime.now().strftime('%Y/%m/%d')
+        # print(date_today)
+        # check_path = os.path.join(settings.MEDIA_ROOT, 'campaigns/ads/ad_images/{date_today}/{ad_image_name}'.format(
+        #     date_today=date_today,
+        #     ad_image_name=ad_image_name
+        # ))
+        # if os.path.exists(check_path):
+        #     media_url = os.path.relpath(check_path, settings.MEDIA_ROOT)
+        #     image_url = os.path.join(settings.MEDIA_URL, media_url)
+        #     print('EXISTS')
+        #     return JsonResponse({ 'image_url': image_url})
+
+        # get current time in hours, minutes and seconds as a string
+        file_extension = ad_image.name.split('.')[-1]
+        current_time = datetime.now().strftime('%H%M%S')
+        new_file_name = ad_image_name + '_' + current_time + '.' + file_extension
+        # save the image to the media folder
+        new_ad = ad.objects.create(main_image=ad_image)
+        new_ad.save()
+        print(new_ad.main_image.url)
+        return JsonResponse({ 'image_url': new_ad.main_image.url, 'image_id': new_ad.id })
+    else:
+        return redirect('/')
+
+
+def upload_campaign_video(request):
+    if request.method == 'POST':
+        ad_video = request.FILES['ad_video']
+        ad_video_name = ad_video.name
+        # get current time in hours, minutes and seconds as a string
+        file_extension = ad_video.name.split('.')[-1]
+        current_time = datetime.now().strftime('%H%M%S')
+        new_file_name = ad_video_name + '_' + current_time + '.' + file_extension
+        # save the image to the media folder
+        new_ad = ad.objects.create(main_video=ad_video)
+        new_ad.save()
+        
+
+        return JsonResponse({
+             'video_url': new_ad.main_video.url, 
+             'video_id': new_ad.id,})
+    else:
+        return redirect('/')
+    
+
+def get_video_thumbnails(request):
+    if request.method == 'GET':
+        thumbnail_urls = []
+        ad_id = int(request.GET.get('ad_id'))
+        old_ad = ad.objects.get(id=ad_id)
+        video_file_path = old_ad.main_video.path
+        new_file_name = old_ad.video_filename.split('.')[0]
+        with VideoFileClip(video_file_path) as video:
+            video_width, video_height = video.size
+            for i in range(6):
+                thumbnail = video.get_frame(random.uniform(0, video.duration))
+                thumbnail = Image.fromarray(thumbnail)
+                thumbnail = thumbnail.resize((video_width, video_height))
+                thumbnail_name = new_file_name + '_thumbnail_' + str(i+1) + '.png'
+                thumbnail_dir = os.path.join(settings.MEDIA_ROOT, 'campaigns', 'ads', 'ad_videos', 'thumbnails', datetime.now().strftime('%Y/%m/%d'))
+                thumbnail_path = os.path.join(thumbnail_dir, thumbnail_name)
+                os.makedirs(thumbnail_dir, exist_ok=True)
+                thumbnail.save(thumbnail_path, 'PNG')
+                thumbnail_url = os.path.join(settings.MEDIA_URL, 'campaigns', 'ads', 'ad_videos', 'thumbnails', datetime.now().strftime('%Y/%m/%d'), thumbnail_name)
+                thumbnail_urls.append(thumbnail_url)
+        return JsonResponse({
+                'thumbnail_urls': thumbnail_urls,
+        })
+    else:
+        return redirect('/')
+
+    
+def generate_ad_video_thumbnail(request):
+    if request.method == 'GET':
+        ad_id = int(request.GET.get('ad_id'))
+        current_time = float(request.GET.get('current_time'))
+        old_ad = ad.objects.get(id=ad_id)
+        video_file_path = old_ad.main_video.path
+        new_file_name = old_ad.video_filename.split('.')[0]
+        with VideoFileClip(video_file_path) as video:
+            video_width, video_height = video.size
+            thumbnail = video.get_frame(current_time)
+            thumbnail = Image.fromarray(thumbnail)
+            thumbnail = thumbnail.resize((video_width, video_height))
+            thumbnail_name = new_file_name + '_thumbnail' + str(int(current_time)) + '.png'
+            thumbnail_dir = os.path.join(settings.MEDIA_ROOT, 'campaigns', 'ads', 'ad_videos', 'thumbnails', datetime.now().strftime('%Y/%m/%d'))
+            thumbnail_path = os.path.join(thumbnail_dir, thumbnail_name)
+            os.makedirs(thumbnail_dir, exist_ok=True)
+            thumbnail.save(thumbnail_path, 'PNG')
+            thumbnail_url = os.path.join(settings.MEDIA_URL, 'campaigns', 'ads', 'ad_videos', 'thumbnails', datetime.now().strftime('%Y/%m/%d'), thumbnail_name)
+        return JsonResponse({
+                'thumbnail_url': thumbnail_url
+        })
+    else:
+        return redirect('/')
+
+
+def upload_video_thumbnail(request):
+    if request.method == 'POST':
+        ad_image = request.FILES['ad_image']
+        new_ad = ad.objects.create(video_thumbnail=ad_image)
+        new_ad.save()
+        return JsonResponse({ 'image_url': new_ad.video_thumbnail.url})
+    else:
+        return redirect('/')
+
+
+
+def search_ad_audiences(request):
+    if request.method == 'GET':
+        search_term = str(request.GET.get('search_term'))
+        audiences = ad_campaigns.search_ad_interests(request, search_term)
+        return JsonResponse({ 'audiences': audiences })
+        
+
+    else:
+        return redirect('/')
+
+
+
+def create_campaign(request):
+    if request.method == 'POST':
+        campaign_name = request.POST.get('campaign_name')
+        product_id = request.POST.get('product_id')
+        link_url = 'https://greengoshop.mk' + Product.objects.get(id=product_id).get_absolute_url()
+        print(link_url)
+        adsets = json.loads(request.POST.get('adsets'))
+        
+        
+        campaign = ad_campaigns.create_facebook_campaign(campaign_name=campaign_name)
+        campaign_id = campaign['id']
+        print(campaign)
+        
+
+
+        for adset in adsets:
+            adset_name = adset['adset_name']
+            budget = int(adset['adset_budget'])
+            min_age = int(adset['adset_minage'])
+            max_age = int(adset['adset_maxage'])
+            audience_id = adset['adset_audience_id']
+            audience_name = adset['adset_audience_name']
+            
+            if "genders" in adset:
+                genders = adset['genders']
+            budget = budget * 100
+            created_adset = ad_campaigns.create_facebook_adset(campaign_id=campaign_id, name=adset_name, budget=budget, max_age = max_age,
+                                                       min_age = min_age, interest_id=audience_id, interest_name=audience_name)
+            created_adset_id = created_adset['id']
+            for ad in adset['ads']:
+                ad_image = ''
+                ad_video = ''
+                ad_thumbnail = ''
+                ad_name = ad['ad_name']
+                ad_primary = ad['ad_primary_text']
+                ad_headline = ad['ad_headline']
+                ad_description = ad['ad_description']
+                ad_type = ad['ad_type']
+
+                if ad_type == 'photo':
+                    ad_image = ad['ad_image_path']
+                    created_ad = ad_campaigns.create_facebook_ad(ad_set_id=created_adset_id, ad_type='image', ad_name=ad_name,ad_primary_text=ad_primary,
+                                                                 ad_description_text=ad_description, ad_headline_text=ad_headline, ad_image=ad_image, ad_link_url=link_url)
+                    
+                elif ad_type == 'video':
+                    ad_video = ad['ad_video_path']
+                    ad_thumbnail = ad['thumbnail_path']
+                    print(ad_video, ad_thumbnail)
+                    print('CREEATE AD CALLED')
+                    created_ad = ad_campaigns.create_facebook_ad(ad_set_id=created_adset_id, ad_type='video', ad_name=ad_name,ad_primary_text=ad_primary,
+                                                                 ad_description_text=ad_description, ad_headline_text=ad_headline, ad_video = ad_video, ad_thumbnail = ad_thumbnail, ad_link_url=link_url)
+
+
+
+        return JsonResponse({ 'campaign_id': campaign_id })
+    else:
+        return redirect('/')
+        
+
+
+def save_new_campaign_id(request):
+    if request.method == 'POST':
+        campaign_id = str(request.POST.get('campaign_id'))
+        prod_id = request.POST.get('product_id')
+        product = Product.objects.get(id=prod_id)
+        product_campaigns.objects.create(product=product, campaign_id=campaign_id)
+
+        return JsonResponse({'success': 'success'})
+
+    else:
+        redirect('/')
+
+
+
