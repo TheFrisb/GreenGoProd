cb7c4d445ab3b523265d43c622c5138092d52cae
Aleksandar <97000339+TheFrisb@users.noreply.github.com>
2023-04-22 02:51:51 +0200

    Update cron.py

--- shop/cron.py
+++ shop/cron.py
@@ -6,3 +6,14 @@ from django.utils import timezone
 def old_carts_cleaner():
     two_days_ago = timezone.now() - timezone.timedelta(days=2)
     Cart.objects.filter(created_at__lt=two_days_ago).delete()
+
+    
+def reverse_offer_if_older_than_1_day():
+    # get all carts older than 1 day
+    one_day_ago = timezone.now() - timezone.timedelta(days=1)
+    carts = Cart.objects.filter(created_at__lt=one_day_ago)
+    for cart in carts:
+        cart.has_viewed_checkout_offer = False
+        cart.has_viewed_checkout_offer_time = None
+        cart.has_accepted_checkout_offer = False
+        cart.save()
